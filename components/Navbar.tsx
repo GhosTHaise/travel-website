c672b41516b832d36163755b192b91fba798941d
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-10-20 17:15:48 +0300

    show navabr logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header>
+        <nav className='flexBetween  max-container 
+        padding-container relative z-30 py-5'>
+            <Link
+                className=''
+                href={"/"}
+            >
+                <Image
+                    src="/hilink-logo.svg"
+                    alt='logo'
+                    width={74}
+                    height={29}
+                />
+            </Link>
+        </nav>
+    </header>
   )
 }
 
