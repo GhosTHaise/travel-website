3c6fe86b74415b88c5e2bb815692e33a1b2250f5
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-10-22 19:11:33 +0300

    build campsite Components

---
+++ components/CampSite.tsx
@@ -0,0 +1,22 @@
+
+type Props = {
+    backgroundImage : string;
+    title : string;
+    sibtitle : string;
+    peopleJoined : string;
+}
+
+const CampSite = ({
+    backgroundImage,
+    title,
+    sibtitle,
+    peopleJoined
+} : Props) => {
+  return (
+    <div>
+        CampSite1
+    </div>
+  )
+}
+
+export default CampSite
--- components/Home/Camp.tsx
+++ components/Home/Camp.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import CampSite from '../CampSite'
 
 const Camp = () => {
   return (
-    <div>Camp</div>
+    <section
+      className='border-2 border-green-500 2xl:max-container relative 
+      flex flex-col py-10 lg:md-10 lg:py-20 xl:mb-20'
+    >
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start
+       gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+          <CampSite
+            backgroundImage="bg-img-1"
+            title="Putuk Truno Camp"
+            sibtitle="Prigen, Pasuruan"
+            peopleJoined="50+ joined"
+          />
+          <CampSite
+            backgroundImage="bg-img-2"
+            title="Mountain View Camp"
+            sibtitle="Somewhere in the Wilderness"
+            peopleJoined="30+ joined"
+          />
+      </div>
+    </section>
   )
 }
 
