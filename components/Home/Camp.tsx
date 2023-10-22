65558027607c25cb0cf630cee43c825a41964541
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-10-22 20:14:43 +0300

    build image-slide

--- components/Home/Camp.tsx
+++ components/Home/Camp.tsx
@@ -4,21 +4,21 @@ import CampSite from '../CampSite'
 const Camp = () => {
   return (
     <section
-      className='border-2 border-green-500 2xl:max-container relative 
+      className='2xl:max-container relative 
       flex flex-col py-10 lg:md-10 lg:py-20 xl:mb-20'
     >
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start
        gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
           <CampSite
-            backgroundImage="bg-img-1"
+            backgroundImage="bg-bg-img-1"
             title="Putuk Truno Camp"
-            sibtitle="Prigen, Pasuruan"
+            subtitle="Prigen, Pasuruan"
             peopleJoined="50+ joined"
           />
           <CampSite
-            backgroundImage="bg-img-2"
+            backgroundImage="bg-bg-img-2"
             title="Mountain View Camp"
-            sibtitle="Somewhere in the Wilderness"
+            subtitle="Somewhere in the Wilderness"
             peopleJoined="30+ joined"
           />
       </div>
