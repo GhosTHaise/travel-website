52655cccabe4b5f4430e9d567f0a36176259b043
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-10-24 23:34:23 +0300

    Update Guide.tsx

--- components/Home/Guide.tsx
+++ components/Home/Guide.tsx
@@ -23,16 +23,45 @@ const Guide = () => {
             <div
               className='flex flex-wrap justify-between gap-5 lg:gap-10'
             >
-                <h2>
+                <h2 className='bold-40 lg:bold-64 xl:max-w-[390px]'>
                   Guide You to Easy Path
                 </h2>
-                <p>
+                <p className='regular-16 text-gray-30 xl:max-w-[520px]'>
                     Only with the hilink application you will no longer 
                     get lost and get lost again, because we already support 
                     offline maps when there is no internet connection in the field. 
                     Invite your friends, relatives and friends to have fun in the 
                     wilderness through the valley and reach the top of the mountain.
                 </p>
+            </div>
+        </div>
+        
+        <div className='flexCenter  max-container relative w-full border-2 border-blue-500'>
+            <Image
+              src="/boat.png"
+              alt='boat'
+              width={1440}
+              height={580}
+              className='w-full  object-cover object-center 2xl:rounded-5xl'
+            />
+
+            <div
+              className='absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl
+               border shadow-md md:left-[5%] lg:top-20'
+            >
+                <div
+                  className='flexBetween flex-col'
+                >
+                  <div
+                    className='flex w-full flex-col'
+                  >
+                      <div
+                        className='flexBetween w-full'
+                      >
+
+                      </div>
+                  </div>
+                </div>
             </div>
         </div>
     </section>
